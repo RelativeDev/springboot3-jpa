0a6505eab295edf891949065c746cbba9492b78e
Pedro Gomes <pedrogomespeixoto14@gmail.com>
2022-10-21 08:15:10 -0300

    JPArepository

---
+++ src/main/java/com/springboot/firstproject/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.springboot.firstproject.repositories;
+
+import com.springboot.firstproject.entities.User1;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User1, Long> {
+}
